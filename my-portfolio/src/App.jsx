4f144fab661ed46c49179427f67ed8d337bbd21d
Kanishk Singh <oscoro.graves@gmail.com>
2026-01-12 19:08:23 +0530

    Upgrade to Firefly background and fix z-index layering

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1413,6 +1413,7 @@ export default function Portfolio() {
         <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-blue-300/30 dark:bg-blue-900/20 rounded-full blur-3xl animate-pulse" style={{ animationDuration: '10s', animationDelay: '1s' }} />
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-rose-200/20 dark:bg-rose-900/10 rounded-full blur-3xl animate-pulse" style={{ animationDuration: '12s', animationDelay: '2s' }} />
       </div>
+      <CustomCursor />
       <NavBar />
 
       {/* Main Content Grows to fill space */}
